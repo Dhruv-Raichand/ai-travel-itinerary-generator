a319d9bc19c7389015523b24a7937031f77a6549
Dhruv-Raichand <dhruvraichand70@gmail.com>
2026-05-22 21:40:15 +0530

    setup frontend with vite tailwind and shadcn

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-5xl m-5 py-5 text-white">
+        MERN AI Itinerary Generator
+      </h1>
+    </>
+  );
+}
+
+export default App;
